5734c414e53143f4344eb7ee5671d5560f5f678d
Nolan Fuchs <nolan@magneticmobile.com>
2020-11-14 00:27:32 -0800

    Initial Commit

---
+++ Vivid Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vivid Camera
+//
+//  Created by Marxent on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Vivid CameraTests/Vivid_CameraTests.swift
@@ -0,0 +1,33 @@
+//
+//  Vivid_CameraTests.swift
+//  Vivid CameraTests
+//
+//  Created by Marxent on 11/14/20.
+//
+
+import XCTest
+@testable import Vivid_Camera
+
+class Vivid_CameraTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
