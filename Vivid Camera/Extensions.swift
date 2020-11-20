e1557edec9eeb54d2b7929b8fb78c54d292e54c0
Nolan Fuchs <Nolan1235@gmail.com>
2020-11-20 14:22:45 -0800

    Changing url extension name

--- Vivid Camera/Extensions.swift
+++ Vivid Camera/Extensions.swift
@@ -21,7 +21,7 @@ extension URL {
         if let documentDirectory = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
             let formatter = DateFormatter()
             formatter.dateFormat = "yyyyMMddHHmmssSSS"
-            let filePath: String = "\(documentDirectory)/viv-\(formatter.string(from: Date())).mp4"
+            let filePath: String = "\(documentDirectory)/VIVID Camera-\(formatter.string(from: Date())).mp4"
             return URL(fileURLWithPath: filePath)
         }
         return URL(fileURLWithPath: "EmptyFilePath")
